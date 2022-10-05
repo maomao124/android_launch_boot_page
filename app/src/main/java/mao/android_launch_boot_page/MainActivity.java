d151ce99f120d661affa467bd4e923f2597c831f
mao <1296193245@qq.com>
2022-10-05 17:31:25 +0800

    android_launch_boot_page

--- app/src/main/java/mao/android_launch_boot_page/MainActivity.java
+++ app/src/main/java/mao/android_launch_boot_page/MainActivity.java
@@ -1,8 +1,11 @@
 package mao.android_launch_boot_page;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import mao.android_launch_boot_page.adapter.LaunchAdapter;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +15,9 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.ViewPager);
+        LaunchAdapter launchAdapter = new LaunchAdapter(this);
+        viewPager.setAdapter(launchAdapter);
     }
 }
